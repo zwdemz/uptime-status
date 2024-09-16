325cd9dcf9becb02a0513d57f153c52d308aa6b2
.BeiCheng. <73386299+zwdemz@users.noreply.github.com>
2024-09-16 12:11:34 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'Yki-yawa Site Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2678557-d4740f39436a0a26b9eb676a',
   ],
 
   // 日志天数
@@ -26,11 +23,11 @@ window.Config = {
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/zwdemz/uptime-status'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://zwdemz.netlify.app'
     },
   ],
 };
